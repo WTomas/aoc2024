3674aa0989bf43a673bac4c20bc23df3b681c69b
WTomas <vajdatomi98@gmail.com>
2024-12-07 12:17:27 +0000

    add: day01

---
+++ challenges/Day01.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import Data.List (transpose, sort)
+import Utils ( printSolution1, printSolution2, makeCounter, (!) )
+
+readInt :: String -> Int
+readInt = read
+
+getDistance :: [Int] -> Int
+getDistance (a:b:[]) = abs (a - b)
+getDistance _ = 0
+
+solution1 :: String -> Int
+solution1 input = sum . map getDistance . transpose . map sort . map (map readInt) . transpose . (map words . lines) $ input
+
+solution2 :: String -> Int
+solution2 input = do 
+    let first:second:[] = map (map readInt) . transpose . (map words . lines) $ input
+    sum . map (uncurry (*)) $ zip (map ((!) (makeCounter second)) first) first
+
+main :: IO()
+main = do
+    input <- readFile "inputs/day01.txt"
+    printSolution1 $ solution1 input
+    printSolution2 $ solution2 input
---
+++ challenges/Utils.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
+module Utils where
+
+import qualified Data.Map as M
+import Data.Maybe (fromMaybe)
+
+printSolution:: (Show a) => Int ->  a -> IO()
+printSolution n solution = putStrLn $ "Solution " ++ show n ++ ": "  ++ show solution
+
+printSolution1:: (Show a) => a -> IO()
+printSolution1 = printSolution 1
+
+printSolution2:: (Show a) => a -> IO()
+printSolution2 = printSolution 2
+
+data (Ord a) => Counter a = Counter {
+    _map :: M.Map a Int 
+}
+
+(!) :: (Ord a) => Counter a -> a -> Int
+(Counter { _map }) ! element = fromMaybe 0 $ element `M.lookup` _map
+
+incrementCounter :: (Ord a) => Counter a -> a -> Counter a
+incrementCounter (counter@Counter { _map }) element = Counter { _map = M.insert element (counter ! element + 1) _map }
+
+makeCounter :: (Ord a) => [a] -> Counter a
+makeCounter as = foldl incrementCounter Counter { _map = M.empty } as
+
+instance (Ord a, Show a) => Show (Counter a) where
+    show (Counter { _map }) = show _map
+
+
