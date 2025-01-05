ecaff3db58a1c0b1591b7d75b28d25c07d1c029e
WTomas <vajdatomi98@gmail.com>
2025-01-05 11:24:35 +0100

    add: day09/2

--- challenges/Day09.hs
+++ challenges/Day09.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import ChallengeUtils (readInt, replaceAt, printSolution1)
+import ChallengeUtils (readInt, replaceAt, printSolution1, printSolution2)
 import Data.Char (digitToInt)
 import Data.Foldable (find)
 import Data.List (findIndex, groupBy)
@@ -13,11 +13,20 @@ isFile :: DiskAllocation -> Bool
 isFile FreeSpace = False
 isFile (File _) = True
 
+calculateFreeSpace :: DiskSpace -> Int
+calculateFreeSpace (DiskSpace _ allocation) = length $ filter (not . isFile) allocation 
+
+calculateFileSize :: DiskSpace -> Int
+calculateFileSize (DiskSpace _ allocation) = length $ filter isFile allocation 
+
 isDiskSpaceFull :: DiskSpace -> Bool
-isDiskSpaceFull (DiskSpace size allocation) = size == (length $ filter isFile allocation)
+isDiskSpaceFull = (==) 0 . calculateFreeSpace
 
 isDiskSpaceEmpty :: DiskSpace -> Bool
-isDiskSpaceEmpty (DiskSpace size allocation) = size == (length $ filter (not . isFile) allocation)
+isDiskSpaceEmpty diskSpace@(DiskSpace size allocation) = size == calculateFreeSpace diskSpace
+
+getFileId :: DiskSpace -> Maybe Int
+getFileId (DiskSpace _ allocation) = (\(File fileId) -> fileId) <$> find isFile allocation
 
 removeFileAllocation :: DiskSpace -> (DiskSpace, DiskAllocation)
 removeFileAllocation (DiskSpace size allocations) = do 
@@ -41,6 +50,12 @@ transferFileAllocation fromDiskSpace toDiskSpace = do
     let toDiskSpace' = addFileAllocation file toDiskSpace
     (fromDiskSpace', toDiskSpace')
 
+transferFile :: DiskSpace -> DiskSpace -> (DiskSpace, DiskSpace)
+transferFile fromDiskSpace toDiskSpace
+    | calculateFreeSpace toDiskSpace >= calculateFileSize fromDiskSpace = do
+        iterate (uncurry transferFileAllocation) (fromDiskSpace, toDiskSpace) !! (calculateFileSize fromDiskSpace)
+    | otherwise = (fromDiskSpace, toDiskSpace) 
+
 orderFileAllocation :: DiskSpace -> DiskSpace
 orderFileAllocation (DiskSpace size allocation) = DiskSpace size ((filter isFile allocation) ++ (filter (not . isFile) allocation))
 
@@ -55,6 +70,26 @@ transfer diskSpaces = do
             True -> do
                 let (newNonEmptySpace, newFreeSpace) = transferFileAllocation nonEmptySpace freeSpace
                 transfer . (replaceAt nonEmptySpaceIndex newNonEmptySpace) . (replaceAt freeSpaceIndex newFreeSpace) $ diskSpaces
+
+transferFiles :: [DiskSpace] -> [DiskSpace]
+transferFiles diskSpaces = foldr f diskSpaces diskSpaces
+    where 
+        f :: DiskSpace -> [DiskSpace] -> [DiskSpace]
+        f diskSpace diskSpaces'
+            | isDiskSpaceEmpty diskSpace = diskSpaces'
+            | otherwise = do
+                let freeSpaceIdx = fromMaybe (-1) $ findIndex (\ds -> calculateFreeSpace ds >= calculateFileSize diskSpace) diskSpaces'
+                let fileSpaceIdx = fromMaybe (-2) $ findIndex (\ds -> getFileId ds == getFileId diskSpace) diskSpaces'
+                case freeSpaceIdx < 0 of 
+                    True -> diskSpaces'
+                    False -> do
+                        case fileSpaceIdx < 0 of 
+                            True -> diskSpaces'
+                            False -> do
+                                case freeSpaceIdx < fileSpaceIdx of
+                                    True -> let (from', to') = transferFile diskSpace (diskSpaces' !! freeSpaceIdx)
+                                        in (replaceAt freeSpaceIdx to') . (replaceAt fileSpaceIdx from') $ diskSpaces'
+                                    False -> diskSpaces'
     
 consumeDiskMap :: Int -> Bool -> [Int] -> [DiskSpace] -> (Int, Bool, [Int], [DiskSpace])
 consumeDiskMap idNumber isFile [] diskSpace = (idNumber, isFile, [], reverse diskSpace)
@@ -62,20 +97,33 @@ consumeDiskMap idNumber True (d:diskMap) diskSpace = consumeDiskMap (idNumber+1)
 consumeDiskMap idNumber False (d:diskMap) diskSpace = consumeDiskMap idNumber True diskMap ((DiskSpace d (take d $ repeat FreeSpace)):diskSpace)
 
 checkSum :: [DiskSpace] -> Int
-checkSum diskSpace = sum 
-    $ map (uncurry (*)) 
-    $ zip [0..] $ map (\(File idNumber) -> idNumber) 
-    $ filter isFile 
-    $ concat 
-    $ map (\(DiskSpace _ allocation) -> allocation) diskSpace
+checkSum  = sum 
+    . map (uncurry (*)) 
+    . zip [0..] 
+    . map allocationValue
+    . concat 
+    . map (\(DiskSpace _ allocation) -> allocation)
+    where 
+        allocationValue :: DiskAllocation -> Int
+        allocationValue FreeSpace = 0
+        allocationValue (File x) = x
 
+    
 solution1 :: String -> Int
 solution1 input = do
     let diskMap = map digitToInt input 
     let (_, _, _, diskSpace) = consumeDiskMap 0 True diskMap []
     checkSum $ transfer diskSpace
 
+solution2 :: String -> Int
+solution2 input = do
+    let diskMap = map digitToInt input 
+    let (_, _, _, diskSpace) = consumeDiskMap 0 True diskMap []
+    checkSum $ transferFiles diskSpace
+
+
 main :: IO()
 main = do
     input <- readFile "inputs/day09.txt"
     printSolution1 $ solution1 input
+    printSolution2 $ solution2 input
